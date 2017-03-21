c69c0b584f14b3a36876a1289e263424957de18f
Pivotal <pivotal@utica.ny.pivotallabs.com>
2017-03-21 17:58:44 -0400

    WIPWIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   include UserControllerHelper
   before_action :authenticate_user!, except: [:demo, :addresses]
-  before_action :authenticate_admin_user!, only: [:edit, :update]
+  before_action :authenticate_admin_user!, only: [:new, :edit, :update]
   before_action :load_user, only: [:edit, :update]
 
   def update
@@ -162,12 +162,17 @@ class UsersController < ApplicationController
         :phone_number,
         :zip_code,
         :permissions,
-        :twine_name
+        :twine_name,
+        :apartment,
+        :password,
+        :password_confirmation,
+        :permissions,
+        :sensor_codes_string
       ])
     end
 
     def authenticate_admin_user!
-      return if current_user.permissions <= User::PERMISSIONS[:admin]
+      return if current_user.permissions <= User::PERMISSIONS[:super_user]
       redirect_to root_path
     end
 
